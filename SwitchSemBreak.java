aa784e73411df6bb68482d2fd406c6c13802a624
FilipeDawra <filipedawra@gmail.com>
2025-11-18 15:25:32 -0300

    Switch sem Break

---
+++ SwitchSemBreak.java
@@ -0,0 +1,24 @@
+package controle;
+
+public class SwitchSemBreak {
+	public static void main(String[] args) {
+		
+		int idade = 0;
+		switch(idade) {
+		case(5):
+			System.out.println("Sabe tocar");
+		case(4):
+			System.out.println("Sabe cantar");
+		case(3):
+			System.out.println("Sabe correr");
+		case(2):
+			System.out.println("Sabe andar");
+		case(1):
+			System.out.println("Sabe engatinhar");
+		case(0):
+			System.out.println("Sabe respirar");			
+		}
+		}
+	}
+
+
