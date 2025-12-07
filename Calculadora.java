a46436fd2de00904e76f53f992c1ebd32cd291c4
FilipeDawra <filipedawra@gmail.com>
2025-12-07 06:36:15 -0300

    feat: add basic calculator with infinite loop

--- Calculadora.java
+++ Calculadora.java
@@ -9,33 +9,37 @@ public class Calculadora {
 
         Scanner entrada = new Scanner(System.in);
 
-
+        do {
             System.out.println("Digite o primeiro número");
             a = entrada.nextDouble();
             System.out.println("Digite o segundo número");
             b = entrada.nextDouble();
             System.out.println("Escolha o tipo de operação: + , -, *, /. ");
             operador = entrada.next();
-        switch (operador) {
-            case "+":
-                System.out.println("Resultado: " + (a + b));
+            if (operador.equals("sair")) {
                 break;
-            case "-":
-                System.out.println("Resultado: " + (a - b));
-                break;
-            case "*":
-                System.out.println("Resultado: " + (a * b));
-                break;
-            case "/":
-                if (a == 0 || b == 0) {
-                    System.out.println("Divisão por zero não permitida");
-                } else {
-                    System.out.println("Resultado: " + (a / b));
-                }
-                break;
-            default:
-                System.out.println("Operador inválido.");
-        }
+            }
+            switch (operador) {
+                case "+":
+                    System.out.println("Resultado: " + (a + b));
+                    break;
+                case "-":
+                    System.out.println("Resultado: " + (a - b));
+                    break;
+                case "*":
+                    System.out.println("Resultado: " + (a * b));
+                    break;
+                case "/":
+                    if (a == 0 || b == 0) {
+                        System.out.println("Divisão por zero não permitida");
+                    } else {
+                        System.out.println("Resultado: " + (a / b));
+                    }
+                    break;
+                default:
+                    System.out.println("Operador inválido.");
+            }
+        }while (!operador.equals("sair"));
             entrada.close();
         }
 
